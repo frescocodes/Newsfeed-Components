fb045b555f8e1d9ca02680a9d36bd547e611f946
Fresco Fresco <fresco@Frescos-MacBook-Pro.local>
2019-09-04 16:56:33 -0400

    created article elements, assigned classes

--- Article/Article.js
+++ Article/Article.js
@@ -112,3 +112,22 @@ const data = [
   Step 5: Add a new article to the array. Make sure it is in the same format as the others. Refresh the page to see the new article.
 
 */
+
+function createArticles(data) {
+  // create elements
+  const article = document.createElement('div');
+  const h2 = document.createElement('h2');
+  const date = document.createElement('p');
+  const p1 = document.createElement('p');
+  const p2 = document.createElement('p');
+  const p3 = document.createElement('p');
+  const expandButton = document.createElement('span');
+  // assign classes
+  article.classList.add('article');
+  date.classList.add('date');
+  expandButton.classList.add('expandButton');
+  // structure elements
+
+}
+
+
